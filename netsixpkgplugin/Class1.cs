6cf1d3bfdd3a9d9428cc982d2600e72016a794da
mjkkirschner <mjk.kirschner@gmail.com>
2023-10-24 17:46:46 -0400

    Add project files.

---
+++ netsixpkgplugin/Class1.cs
@@ -0,0 +1,37 @@
+﻿using Dynamo.Extensions;
+
+namespace netsixpkgplugin
+{
+    public class IsolatedExtension: Dynamo.Extensions.IExtension
+    {
+        public string UniqueId => "cbf67bd6-4af6-46b6-99ed-73218c9407b8";
+
+        public string Name => "IsolatedExtension";
+
+        RestSharp.RestClient RestSharpRestClient { get; set; }
+        public void BuildClient()
+        {
+            RestSharpRestClient = new RestSharp.RestClient();
+        }
+
+        public void Dispose()
+        {
+            RestSharpRestClient.Dispose();
+        }
+
+        public void Ready(ReadyParams sp)
+        {
+            BuildClient();
+        }
+
+        public void Shutdown()
+        {
+            RestSharpRestClient.Dispose();
+        }
+
+        public void Startup(StartupParams sp)
+        {
+            BuildClient();
+        }
+    }
+}
